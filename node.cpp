773a8453124ab1eaa13b7152055c50fe29df21ed
Yat <ylui626@gmail.com>
2022-02-09 18:47:55 -0600

    add traverse_de for getting symbol from codeword

--- node.cpp
+++ node.cpp
@@ -1,28 +1,51 @@
 #include "node.h"
 #include <algorithm>
+#include <bits/types/FILE.h>
 #include <cstdint>
+#include <cstdio>
+#include <sys/types.h>
 #define SIZE 256
 
 uint8_t *codewordTable = new uint8_t[SIZE];
-unsigned char* x = std::fill_n(codewordTable, SIZE, 1);
+unsigned char *x = std::fill_n(codewordTable, SIZE, 1);
+int numberofSymbol = 7;
 
-void Node::traverse( uint8_t progress) {
+void Node::traverse(uint8_t progress) {
   if (left == NULL && right == NULL) {
     codewordTable[symbol] = progress;
   }
 
   uint8_t goLeft = (progress << 1) | 1;
-  uint8_t goRight = progress << 1;
+  uint8_t goRight = (progress << 1) | 0;
 
   if (left != NULL) {
-    left->traverse( goLeft);
+    left->traverse(goLeft);
   }
   if (right != NULL) {
-    right->traverse( goRight);
+    right->traverse(goRight);
   }
 }
 
-
+uint8_t traverse_de(Node *root, uint8_t byte) {
+  static Node *currentPtr = root;
+  int count = 8;
+  while (count >0 && numberofSymbol > 0 ) {
+    uint8_t lsb = byte & 1;
+    byte = byte >> 1;
+    if (lsb == 0) {
+      currentPtr = currentPtr->right;
+    } else {
+      currentPtr = currentPtr->left;
+    }
+    if (currentPtr->left == NULL && currentPtr->right == NULL) {
+      cout << unsigned(currentPtr->symbol) << endl;
+      numberofSymbol --;
+      currentPtr = root;
+    }
+    count --;
+  }
+  return 1;
+}
 
 Node *merge(Node *leftChild, Node *rightChild) {
   Node *result =
@@ -43,7 +66,11 @@ int main() {
   Node *ABCD = merge(BCD, testA);
 
   ABCD->traverse(0);
-  for(int i = 0 ; i < 255 ; i++){
+  for (int i = 0; i < 255; i++) {
     cout << std::bitset<8>(codewordTable[i]) << endl;
   }
+  uint8_t testByte1 = 254;
+  uint8_t testByte2 = 5;
+  traverse_de(ABCD, testByte1);
+  traverse_de(ABCD, testByte2);
 }
